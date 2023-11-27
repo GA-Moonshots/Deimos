dee0c7515ce36826bd92a2270da51eb395269a88
Dan Adiletta <dan.adiletta@gmail.com>
2023-11-27 09:08:34 -0500

    removed strange duplicate of code

--- TeamCode/src/java/org/firstinspires/ftc/teamcode/Constants.java
+++
@@ -1,6 +0,0 @@
-package org.firstinspires.ftc.teamcode;
-
-public class Constants {
-    // Generalized minimum input for the robot to respond to controller input
-    public static final double INPUT_THRESHOLD = 0.1;
-}
--- TeamCode/src/java/org/firstinspires/ftc/teamcode/wrappers/HexMotor.java
+++
@@ -1,8 +0,0 @@
-package org.firstinspires.ftc.teamcode.wrappers;
-
-import com.qualcomm.hardware.motors.RevRoboticsCoreHexMotor;
-import com.qualcomm.robotcore.hardware.DcMotor;
-
-public interface HexMotor extends DcMotor, RevRoboticsCoreHexMotor {
-    // Inteface to merge functionality bewteen both
-}
